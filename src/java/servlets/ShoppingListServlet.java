1c0add225aaf5572fa0ca5dea38a374ef0ed3c75
user <user@10.0.0.176>
2022-10-09 19:12:16 -0600

    Servlet almost completed. Finished doGet method of servlet.

--- src/java/servlets/ShoppingListServlet.java
+++ src/java/servlets/ShoppingListServlet.java
@@ -1,4 +1,3 @@
-
 package servlets;
 
 import java.io.IOException;
@@ -7,6 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -14,17 +14,36 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ShoppingListServlet extends HttpServlet {
 
-   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      
+
+        HttpSession session = request.getSession();
+        String action = request.getParameter("action");
+
+        if (action != null && action.equals("logout")) {
+            session.invalidate();
+
+            session = request.getSession();
+            request.setAttribute("message", "Logged out!");
+            getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+
+        }
+
+        String username = (String) session.getAttribute("username");
+
+        if (username != null && action == null) {
+
+            session.setAttribute("username", username);
+            getServletContext().getRequestDispatcher("/WEB-INF/shoppinglist.jsp").forward(request, response);
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
 
-  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         
     }
 
